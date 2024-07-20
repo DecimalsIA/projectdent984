f713d24101a4e1d5360880194f2fbf514ed415b3
Fredy Saul Teheran Tovar <fredyteheran91@gmail.com>
2024-07-19 23:57:11 -0500

    fix

--- src/app/login/[idUser]/page.tsx
+++ src/app/login/[idUser]/page.tsx
@@ -1,23 +1,27 @@
 /* eslint-disable @next/next/no-img-element */
 
 'use client';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { CardPambii, LogoGame } from 'pambii-devtrader-front';
 import ConnectWallet from '@/components/ConnectWallet';
+import { useSearchParams } from 'next/navigation';
 import { useTelegram } from '@/context/TelegramContext';
 
 const Page: React.FC = () => {
+  const searchParams = useSearchParams();
   const { user: tgUser } = useTelegram();
 
+  const idUser = searchParams.get('idUser');
+  const idP = tgUser?.id ? tgUser?.id : idUser;
   return (
     <main className="flex min-h-screen items-center justify-center p-24">
       <div className="w-[365px] space-y-4 text-center">
         <CardPambii className=" text-white">
           <h2 className="text-xl font-bold mb-4">
-            <span className="title">Sign Up on</span> <p>{tgUser?.id}</p>
+            <span className="title">Sign Up on</span> <p>{idP}</p>
           </h2>
           <LogoGame className="w-100 h-100 mb-4" />
-          <ConnectWallet idUserTelegram={tgUser?.id} />
+          <ConnectWallet idUserTelegram={idP} />
         </CardPambii>
       </div>
     </main>

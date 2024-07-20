8e0e26a6969b48e3a9d7f39c3c68f368e664d5b1
Fredy Saul Teheran Tovar <fredyteheran91@gmail.com>
2024-07-20 02:41:16 -0500

    Update VerifySession.tsx

--- src/components/VerifySession.tsx
+++ src/components/VerifySession.tsx
@@ -8,27 +8,21 @@ import { useRouter } from 'next/navigation';
 import PambiiLoader from './PambiiLoader';
 
 import { useTelegram } from '@/context/TelegramContext';
-import useBase64 from '@/hooks/useBase64';
 
 const VerifySession = () => {
   const { setAuthenticated } = useAuth();
   const router = useRouter();
   const { user: tgUser } = useTelegram();
-  const { json, decodeFromBase64 } = useBase64();
 
   useEffect(() => {
     const verifySession = async () => {
       const token = localStorage.getItem('authToken');
-      if (token) {
-        decodeFromBase64(token);
-      }
 
       if (!token) {
         setAuthenticated(false);
         router.push('/login');
         return;
       }
-      /*
 
       const response = await fetch('/api/verify-session', {
         method: 'POST',
@@ -39,41 +33,33 @@ const VerifySession = () => {
         body: JSON.stringify({ token }),
       });
 
-    const data = await response.json();
-      console.log('data', data);
-*/
+      const data = await response.json();
 
-      if (token) {
-        if (json.session) {
-          setAuthenticated(true);
-          router.push('/game/home');
-        } else {
+      if (response.ok && data.active) {
+        if (!data.idWallet) {
           setAuthenticated(false);
           const deeplink = `https://phantom.app/ul/browse/https://pambii-front.vercel.app/login/${tgUser?.id}?ref=https://pambii-front.vercel.app`;
-          window.location.href = deeplink;
+          // window.location.href = deeplink;
           console.log('1', deeplink);
+        } else {
+          setAuthenticated(true);
+          router.push('/game/home');
         }
       } else {
+        console.log('data', data);
         setAuthenticated(false);
-        if (json.firstTime) {
+        if (data.firstTime) {
           router.push('/login');
         } else {
           const deeplink = `https://phantom.app/ul/browse/https://pambii-front.vercel.app/login/${tgUser?.id}?ref=https://pambii-front.vercel.app`;
-          window.location.href = deeplink;
+          //window.location.href = deeplink;
           console.log('2', deeplink);
         }
       }
     };
 
     verifySession();
-  }, [
-    decodeFromBase64,
-    json.firstTime,
-    json.session,
-    router,
-    setAuthenticated,
-    tgUser?.id,
-  ]);
+  }, [router, setAuthenticated, tgUser?.id]);
   return <PambiiLoader />;
 };
 

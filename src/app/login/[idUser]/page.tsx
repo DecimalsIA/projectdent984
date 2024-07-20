dda7c84b29bb79d29258ebe117d052ff11d6038e
Fredy Saul Teheran Tovar <fredyteheran91@gmail.com>
2024-07-20 00:17:44 -0500

    Update page.tsx

--- src/app/login/[idUser]/page.tsx
+++ src/app/login/[idUser]/page.tsx
@@ -4,24 +4,20 @@
 import React, { useEffect, useState } from 'react';
 import { CardPambii, LogoGame } from 'pambii-devtrader-front';
 import ConnectWallet from '@/components/ConnectWallet';
-
-import { useTelegram } from '@/context/TelegramContext';
 import { useRouter } from 'next/router';
 const Page: React.FC = () => {
   const router = useRouter();
-  const { user: tgUser } = useTelegram();
-
   const idUser = router.query.idUser;
-  const idP = tgUser?.id ? tgUser?.id : idUser;
+
   return (
     <main className="flex min-h-screen items-center justify-center p-24">
       <div className="w-[365px] space-y-4 text-center">
         <CardPambii className=" text-white">
           <h2 className="text-xl font-bold mb-4">
-            <span className="title">Sign Up on</span> <p>{idP}</p>
+            <span className="title">Sign Up on</span> <p>{idUser}</p>
           </h2>
           <LogoGame className="w-100 h-100 mb-4" />
-          <ConnectWallet idUserTelegram={idP} />
+          <ConnectWallet idUserTelegram={idUser} />
         </CardPambii>
       </div>
     </main>

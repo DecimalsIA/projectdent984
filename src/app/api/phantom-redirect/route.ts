ba13fff693115121471bdd39592bbfd2263b3d1e
Fredy Saul Teheran Tovar <fredyteheran91@gmail.com>
2024-08-08 15:43:32 -0500

    fix key public

--- src/app/api/phantom-redirect/route.ts
+++ src/app/api/phantom-redirect/route.ts
@@ -1,28 +1,64 @@
-// app/api/phantom-redirect/route.ts
 import { NextRequest, NextResponse } from 'next/server';
-import { collection, addDoc } from 'firebase/firestore';
-import { db } from '@/firebase/config';
+import { collection, getDoc, doc, addDoc } from 'firebase/firestore';
+import { db } from '@/firebase/config'; // Asegúrate de que esta ruta sea correcta
+import nacl from 'tweetnacl';
+import bs58 from 'bs58';
 
+function decryptPayload(data: string, nonce: string, sharedSecret: Uint8Array) {
+  const dataBytes = bs58.decode(data);
+  const nonceBytes = bs58.decode(nonce);
+  const decrypted = nacl.box.open.after(dataBytes, nonceBytes, sharedSecret);
+
+  if (!decrypted) {
+    throw new Error('Failed to decrypt payload');
+  }
+
+  return JSON.parse(new TextDecoder().decode(decrypted));
+}
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const session = searchParams.get('session');
   const publicKey = searchParams.get('public_key');
+  const phantomEncryptionPublicKey = searchParams.get('phantom_encryption_public_key');
+  const data = searchParams.get('data');
+  const nonce = searchParams.get('nonce');
 
-  if (session && publicKey) {
-    try {
-      // Guardar la información en Firestore
-      await addDoc(collection(db, 'phantomConnections'), {
-        session,
-        publicKey,
-        createdAt: new Date().toISOString()
-      });
-
-      return NextResponse.json({ message: 'Connection to Phantom Wallet successful!', session, publicKey });
-    } catch (error: any) {
-      return NextResponse.json({ message: 'Failed to save data', error: error.message }, { status: 500 });
-    }
-  } else {
+  if (!session || !publicKey || !phantomEncryptionPublicKey || !data || !nonce) {
     return NextResponse.json({ message: 'Invalid parameters' }, { status: 400 });
+  }
+
+  try {
+    // Recuperar dappKeyPair desde Firestore
+    const keyPairDocRef = doc(collection(db, 'dappKeyPairs'), 'defaultKeyPair');
+    const docSnap = await getDoc(keyPairDocRef);
+
+    if (!docSnap.exists()) {
+      throw new Error('dappKeyPair not found');
+    }
+
+    const storedKeyPair = docSnap.data();
+    const dappKeyPair = {
+      publicKey: bs58.decode(storedKeyPair.publicKey),
+      secretKey: bs58.decode(storedKeyPair.secretKey),
+    };
+
+    const sharedSecretDapp = nacl.box.before(
+      bs58.decode(phantomEncryptionPublicKey),
+      dappKeyPair.secretKey
+    );
+
+    const connectData = decryptPayload(data, nonce, sharedSecretDapp);
+
+    // Guarda la información en Firestore
+    await addDoc(collection(db, 'phantomConnections'), {
+      session: connectData.session,
+      publicKey: connectData.public_key,
+      createdAt: new Date().toISOString()
+    });
+
+    return NextResponse.json({ message: 'Connection to Phantom Wallet successful!', session: connectData.session, publicKey: connectData.public_key });
+  } catch (error: any) {
+    return NextResponse.json({ message: 'Failed to save data', error: error.message }, { status: 500 });
   }
 }
--- src/hooks/usePhantomConnect.tsx
+++ src/hooks/usePhantomConnect.tsx
@@ -1,19 +1,44 @@
 import { useState, useEffect } from 'react';
 import nacl from 'tweetnacl';
 import bs58 from 'bs58';
+import { db } from '@/firebase/config'; // Asegúrate de que esta ruta sea correcta
+import { collection, addDoc, getDoc, doc } from 'firebase/firestore';
 
 const usePhantomConnect = () => {
   const [url, setUrl] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
-  const [dappKeyPair] = useState(() => nacl.box.keyPair());
 
   useEffect(() => {
     const generatePhantomConnectUrl = async () => {
       try {
+        let dappKeyPair;
+
+        // Obtener o crear un par de claves
+        const keyPairDocRef = doc(
+          collection(db, 'dappKeyPairs'),
+          'defaultKeyPair',
+        );
+        const docSnap = await getDoc(keyPairDocRef);
+
+        if (docSnap.exists()) {
+          const storedKeyPair = docSnap.data();
+          dappKeyPair = {
+            publicKey: bs58.decode(storedKeyPair.publicKey),
+            secretKey: bs58.decode(storedKeyPair.secretKey),
+          };
+        } else {
+          // Generar un nuevo par de claves y guardarlo en Firestore
+          dappKeyPair = nacl.box.keyPair();
+          await addDoc(collection(db, 'dappKeyPairs'), {
+            publicKey: bs58.encode(dappKeyPair.publicKey),
+            secretKey: bs58.encode(dappKeyPair.secretKey),
+          });
+        }
+
         const appUrl = 'https://pambii-front.vercel.app'; // URL de tu aplicación
         const redirectLink =
           'https://pambii-front.vercel.app/api/phantom-redirect'; // URL de tu redirección en Next.js
-        const cluster = 'devnet'; // o 'testnet', 'devnet'
+        const cluster = 'mainnet-beta'; // o 'testnet', 'devnet'
 
         const publicKey = bs58.encode(dappKeyPair.publicKey);
 
@@ -34,7 +59,7 @@ const usePhantomConnect = () => {
     };
 
     generatePhantomConnectUrl();
-  }, [dappKeyPair]);
+  }, []);
 
   return { url, error };
 };

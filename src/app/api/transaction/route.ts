81f96251112f3027248d4a5b14799cd8b1a8c221
Fredy Saul Teheran Tovar <fredyteheran91@gmail.com>
2024-08-08 11:39:29 -0500

    fix

---
+++ src/app/api/transaction/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from 'next/server';
+import { Connection, PublicKey, Transaction, SystemProgram } from '@solana/web3.js';
+
+const SOLANA_NETWORK = 'https://api.mainnet-beta.solana.com';
+const connection = new Connection(SOLANA_NETWORK);
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const publicKeyStr = searchParams.get('publicKey');
+
+  if (!publicKeyStr) {
+    return NextResponse.json({ error: 'Public key is required' }, { status: 400 });
+  }
+
+  try {
+    const publicKey = new PublicKey(publicKeyStr);
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: publicKey,
+        toPubkey: new PublicKey('9nJwpxx1A7yZeVFp5qBHwg5eDSfMjMDyam3ZDFVxmd4Y'), // Reemplaza con la dirección de destinatario
+        lamports: 1000, // Cantidad en lamports (1 SOL = 1,000,000,000 lamports)
+      }),
+    );
+
+    transaction.feePayer = publicKey;
+    const { blockhash } = await connection.getRecentBlockhash();
+    transaction.recentBlockhash = blockhash;
+
+    const serializedTransaction = transaction.serialize({ requireAllSignatures: false }).toString('base64');
+
+    return NextResponse.json({ transaction: serializedTransaction, publicKey: publicKeyStr });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: 'Error generating transaction' }, { status: 500 });
+  }
+}
--- src/app/game/explore/buy/page.tsx
+++ src/app/game/explore/buy/page.tsx
@@ -1,24 +1,56 @@
 'use client';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { useTelegram } from '@/context/TelegramContext';
 import { ButtonPambii } from 'pambii-devtrader-front';
 import TelegramGameApp from '@/components/TelegramGameApp';
 import ScComponent from '@/components/ScComponent';
+const PHANTOM_DEEPLINK_URL = 'https://phantom.app/ul/v1/';
 
 const Game: React.FC = () => {
+  const [publicKey] = useState('PublicKeyFromDatabaseHere');
+  const [deeplink, setDeeplink] = useState('');
+
+  useEffect(() => {
+    const generateDeeplink = async () => {
+      if (!publicKey) {
+        console.error('Public key is not available');
+        return;
+      }
+
+      try {
+        const response = await fetch(`/api/transaction?publicKey=${publicKey}`);
+        const data = await response.json();
+
+        if (data.error) {
+          console.error(data.error);
+          return;
+        }
+
+        const returnUrl = 'https://t.me/PambiiGameBot'; // URL para regresar a la miniaplicación de Telegram
+        const deeplinkUrl = `${PHANTOM_DEEPLINK_URL}signAndSendTransaction?app_url=${encodeURIComponent(
+          returnUrl,
+        )}&transaction=${encodeURIComponent(data.transaction)}`;
+
+        setDeeplink(deeplinkUrl);
+      } catch (error) {
+        console.error('Error generating deeplink', error);
+      }
+    };
+
+    generateDeeplink();
+  }, [publicKey]);
+
   return (
     <div>
       <h1>Pagina </h1>
-      <p>Aquí va el contenido del juego.</p>
-
-      <hr className="mt-10 mb-3" />
       <div>
-        <h1>Perfil de Usuario</h1>
-
-        <p>Contenido del perfil</p>
+        {deeplink && (
+          <a href={deeplink} target="_blank" rel="noopener noreferrer">
+            <button>Sign Transaction</button>
+          </a>
+        )}
       </div>
-      <ScComponent />
       <a
         href="https://phantom.app/ul/browse/https://pambii-front.vercel.app/game/explore/buy?ref=https://pambii-front.vercel.app"
         target="_blank"

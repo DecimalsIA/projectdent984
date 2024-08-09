216758cd726dbd7484fbc54f7b0596eb21c0866f
Fredy Saul Teheran Tovar <fredyteheran91@gmail.com>
2024-08-09 15:18:58 -0500

    Update buyInstruction.ts

--- src/instructions/buyInstruction.ts
+++ src/instructions/buyInstruction.ts
@@ -9,7 +9,7 @@ import * as anchor from '@project-serum/anchor';
 // Reemplaza con los valores proporcionados
 const contractPublicKey = new PublicKey('3SSUkmt5HfEqgEmM6ArkTUzTgQdGDJrRGh29GYyJshfe'); // Contract Program ID
 const tokenAddress = new PublicKey('HPsGKmcQqtsT7ts6AAeDPFZRuSDfU4QaLWAyztrY5UzJ'); // SPL Token Address
-const tokenProgramId = TOKEN_PROGRAM_ID; // Usamos el TOKEN_PROGRAM_ID ya importado
+
 
 // Construye la instrucción para el método `buy` en tu contrato
 export async function buildBuyInstruction(
@@ -29,7 +29,7 @@ export async function buildBuyInstruction(
     { pubkey: user, isSigner: true, isWritable: true }, // El usuario firmará la transacción
     { pubkey: userTokenAccount, isSigner: false, isWritable: true }, // Cuenta de tokens SPL del usuario
     { pubkey: contractTokenAccount, isSigner: false, isWritable: true }, // Cuenta de tokens SPL del contrato
-    { pubkey: tokenProgramId, isSigner: false, isWritable: false }, // Programa de tokens SPL
+    { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false }, // Programa de tokens SPL
   ];
 
   const instruction = new TransactionInstruction({

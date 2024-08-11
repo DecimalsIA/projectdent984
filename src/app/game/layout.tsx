7a92db7dc448b45482a728ac7b96bd9bc7f04d66
Fredy Saul Teheran Tovar <fredyteheran91@gmail.com>
2024-08-10 20:30:23 -0500

    Update layout.tsx

--- src/app/game/layout.tsx
+++ src/app/game/layout.tsx
@@ -2,17 +2,12 @@
 'use client';
 import Footer from '@/components/Footer';
 import TelegramGameApp from '@/components/TelegramGameApp';
-import { useTelegram } from '@/context/TelegramContext';
-import { useEffect } from 'react';
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { setShowBackButton } = useTelegram();
-  useEffect(() => {
-    setShowBackButton(true);
-  }, [setShowBackButton]);
   return (
     <div className="w-full m-0 p-0 min-h-screen bg-cover bg-center flex flex-col items-center justify-between">
       <TelegramGameApp />

70d796a66a0111d0b34ac9d52a83a584d9d869d4
Fredy Saul Teheran Tovar <fredyteheran91@gmail.com>
2024-10-11 04:13:53 -0500

    fix

--- src/components/SelectBeeLosser.tsx
+++ src/components/SelectBeeLosser.tsx
@@ -1,8 +1,14 @@
 import { FunctionComponent } from 'react';
 import styles from './SelectBeeContainer.module.css';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { ButtonPambii, IconPambii } from 'pambii-devtrader-front';
 
 const SelectBeeLosser: FunctionComponent = () => {
+  const router = useRouter();
+  const gotoHome = () => {
+    router.push('/game/home');
+  };
   return (
     <div className={styles.selectbeecontainer}>
       <div className={styles.text}>You lost the battle</div>
@@ -17,6 +23,18 @@ const SelectBeeLosser: FunctionComponent = () => {
       <div className={styles.text}>
         Dont worry, when you lose, you can create a better strategy to beat your
         opponents.
+      </div>
+      <div>
+        {' '}
+        <ButtonPambii
+          color="white"
+          bg="#4e4e4e"
+          className="mb-2"
+          onClick={gotoHome}
+          icon={<IconPambii />}
+        >
+          Go to Home
+        </ButtonPambii>
       </div>
     </div>
   );
--- src/components/SelectBeeWinner.tsx
+++ src/components/SelectBeeWinner.tsx
@@ -1,11 +1,17 @@
 import { FunctionComponent } from 'react';
 import styles from './SelectBeeContainer.module.css';
 import Image from 'next/image';
+import { ButtonPambii, IconPambii } from 'pambii-devtrader-front';
+import { useRouter } from 'next/navigation';
 
 const SelectBeeWinner: FunctionComponent = () => {
+  const router = useRouter();
+  const gotoHome = () => {
+router.push('/game/home');
+  };
   return (
     <div className={styles.selectbeecontainer}>
-      <div className={styles.text}>You won the battle</div>
+      <div className={styles.text}>You win the battle</div>
       <Image
         className={styles.icon}
         width={20}
@@ -13,9 +19,21 @@ const SelectBeeWinner: FunctionComponent = () => {
         alt=""
         src="/assets/attack.svg"
       />
-      <div className={styles.text}>PAMBII WIN: 50</div>);
+      <div className={styles.text}>PAMBII WIN: 50</div>
       <div className={styles.text}>
         Go claim your rewards for being the best bee master.
+      </div>
+      <div>
+        {' '}
+        <ButtonPambii
+          color="white"
+          bg="#4e4e4e"
+          className="mb-2"
+          onClick={gotoHome}
+          icon={<IconPambii />}
+        >
+          Go to Home
+        </ButtonPambii>
       </div>
     </div>
   );
